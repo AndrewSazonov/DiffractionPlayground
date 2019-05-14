8c861e3717a4a3426c8a2382e21161f3dc0320dc
Andrew Sazonov <AndrewSazonov@users.noreply.github.com>
2019-05-14 10:28:35 +0200

    Create PowderPattern examples

---
+++ CrysFML/PowderPattern/SimpleCalcPowder/run_SrTiO3.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+PROGRAM_EXE="simple_calc_powder"
+DATA_DIR_NAME="SrTiO3"
+CFL_FILE="$DATA_DIR_NAME"
+
+cd "${0%/*}/data/$DATA_DIR_NAME"
+../../build/$PROGRAM_EXE $CFL_FILE
+
+read -p "Press any key to exit..."
+exit 0
